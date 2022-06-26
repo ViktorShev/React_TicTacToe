743c799e583fa6a76c4bc28b91ab48ca18af3df9
Viktor <viktross736@hotmail.com>
2022-06-26 19:50:02 +0300

    test PR

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class Square extends React.Component {
     render() {
       return (
         <button className="square">
-          {/* TODO */}
+          {/* TODO dasdsadad*/}
         </button>
       );
     }
